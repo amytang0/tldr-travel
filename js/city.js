21df75618bd195b995234ebc4334afeff6dc7b08
Owen Wang <wangfowen@gmail.com>
2015-07-27 22:06:22 -0700

    fixed some bugs with filtering and permalink

--- js/city.js
+++ js/city.js
@@ -109,22 +109,8 @@
     repopulateItems();
   }
 
-  //perma link assumes max of 62 items and that more items on average will be shown than hidden
-  //will break if scale to allow anyone to add things to lists
-  function convertToSingleChar(n) {
-    //convert number to uppercase letter (A=65, a=97)
-    if (n >= 10 && n < 36) {
-      n += 55;
-      n = String.fromCharCode(n);
-    } else if (n >= 36 && n < 62) {
-      n += 61;
-      n = String.fromCharCode(n);
-    }else if (n >= 62) {
-      console.log("index too large for perma linking: " + n);
-      n = "";
-    }
-
-    return n;
+  function shouldShow(placeObj) {
+    return !placeObj.canceled && !placeObj.initOff && placeObj.toggledOn && (onlyFaves === false || (onlyFaves && placeObj.favorite));
   }
 
   function repopulateItems() {
@@ -133,7 +119,7 @@
 
     //show the markers and list items that are to be shown
     for (var i = 0; i < placeObjs.length; i++) {
-      if (!placeObjs[i].canceled && placeObjs[i].toggledOn && (onlyFaves === false || (onlyFaves && placeObjs[i].favorite))) {
+      if (shouldShow(placeObjs[i])) {
         addMarker(placeObjs[i]);
         show(placeObjs[i].$el);
       } else {
@@ -143,17 +129,13 @@
       }
     }
 
-    var url = "";
-    for (var i = 0; i < hiddenItems.length; i++) {
-      var n = hiddenItems[i];
-      n = convertToSingleChar(n);
-      url += n;
-    }
-
     //set permalink for curated list
+    //limitation in that ids based on index from HTML, so can only ever append new items to HTML and not add in middle or rearrange
+    var url = hiddenItems.join(",");
     setParam("list", url);
   }
 
+  //creates a list item and appends it to $list
   function addToList(place) {
     var $listItem = document.createElement('div');
     $listItem.className = "list-item";
@@ -192,7 +174,8 @@
     });
 
     $list.appendChild($listItem);
-    place.$el = $listItem;
+
+    return $listItem;
   }
 
   var $placesMeta = document.getElementById("places-meta");
@@ -225,48 +208,49 @@
             content: $place.innerHTML,
             index: i,
             canceled: false,
+            toggledOn: true,
             favorite: false
           };
 
-          addToList(placeObj);
+          placeObj.$el = addToList(placeObj);
 
           //if obj is in url hide-list, hide on init
-          var n = convertToSingleChar(i);
           var urlParam = getParam("list");
+          var hideList = urlParam.split(",");
 
-          if (urlParam.indexOf(n) !== -1) {
-            console.log(placeObj.link);
-            placeObj.toggledOn = false;
+          if (hideList.indexOf(i.toString()) !== -1) {
+            placeObj.initOff = true;
             hide(placeObj.$el);
           } else {
-            placeObj.toggledOn = true;
+            placeObj.initOff = false;
             addMarker(placeObj);
           }
 
           placeObjs.push(placeObj);
+
         });
       })($places[i], i);
     }
   }
 
-  function initOptions() {
-    var $options = document.getElementById("options");
-    show($options);
-
+  function initSeeFaves() {
     var $seeFaves = document.getElementById("see-faves");
     $seeFaves.addEventListener("click", function() {
       onlyFaves = $seeFaves.checked;
 
       repopulateItems();
     });
+  }
 
+  function initAdvancedControls($advancedOptions) {
     var $advancedToggle = document.getElementById("advanced-toggle");
-    var $advancedOptions = document.getElementById("advanced-options");
+
     $advancedToggle.addEventListener("click", function() {
       toggle($advancedOptions, $advancedToggle.checked);
     });
+  }
 
-    //TODO: decouple data from rendering
+  function initSort() {
     function sortBy(sort) {
       $list.innerHTML = "";
 
@@ -274,8 +258,8 @@
         for (var type in TYPE_INFO) {
           if (TYPE_INFO.hasOwnProperty(type)) {
             for (var i = 0; i < placeObjs.length; i++) {
-              if (placeObjs[i].type === type) {
-                addToList(placeObjs[i]);
+              if (shouldShow(placeObjs[i]) && placeObjs[i].type === type) {
+                placeObjs[i].$el = addToList(placeObjs[i]);
               }
             }
           }
@@ -293,13 +277,15 @@
         });
 
         for (var i = 0; i < sortedList.length; i++) {
-          addToList(sortedList[i]);
+          if (shouldShow(sortedList[i])) {
+            placeObjs[i].$el = addToList(sortedList[i]);
+          }
         }
       } else if (sort === "price") {
         for (var price = 1; price <= 4; price++) {
           for (var i = 0; i < placeObjs.length; i++) {
-            if (placeObjs[i].price === price) {
-              addToList(placeObjs[i]);
+            if (shouldShow(placeObjs[i]) && placeObjs[i].price === price) {
+              placeObjs[i].$el = addToList(placeObjs[i]);
             }
           }
         }
@@ -310,36 +296,35 @@
     $sort.onchange = function() {
       sortBy($sort.options[$sort.selectedIndex].value);
     };
+  }
 
+  function initFilter($filters, isInt) {
     //click to filter down categories
-    var $filters = $advancedOptions.getElementsByClassName("type-check");
     for (var i = 0; i < $filters.length; i++) {
       (function($filter) {
+        var value = $filter.value;
+        if (isInt) {
+          value = parseInt(value, 10);
+        }
+
         $filter.addEventListener("click", function() {
-          toggleAttr($filter.value, $filter.checked);
+          toggleAttr(value, $filter.checked);
         });
       })($filters[i]);
     }
+  }
 
-    //click to filter down prices
-    var $prices = $advancedOptions.getElementsByClassName("price-check");
-    for (var i = 0; i < $prices.length; i++) {
-      (function($price) {
-        $price.addEventListener("click", function() {
-          toggleAttr(parseInt($price.value, 10), $price.checked);
-        });
-      })($prices[i]);
-    }
-
+  function initReset($types, $prices) {
     var $reset = document.getElementById("reset");
     $reset.addEventListener("click", function() {
       for (var i = 0; i < placeObjs.length; i++) {
+        placeObjs[i].initOff = false;
         placeObjs[i].toggledOn = true;
         placeObjs[i].canceled = false;
       }
 
-      for (var i = 0; i < $filters.length; i++) {
-        $filters[i].checked = true;
+      for (var i = 0; i < $types.length; i++) {
+        $types[i].checked = true;
       }
 
       for (var i = 0; i < $prices.length; i++) {
@@ -348,6 +333,33 @@
 
       repopulateItems();
     });
+  }
+
+  function initSave() {
+    var $save = document.getElementById("save");
+    $save.addEventListener("click", function() {
+      //TODO: open a modal with the current URL in it
+      //TODO: add functionality to export to google maps and maybe also maps.me
+    });
+  }
+
+  //TODO: move each thing into its own function
+  function initOptions() {
+    var $options = document.getElementById("options");
+    show($options);
+
+    initSeeFaves();
+
+    var $advancedOptions = document.getElementById("advanced-options");
+    var $types = $advancedOptions.getElementsByClassName("type-check");
+    var $prices = $advancedOptions.getElementsByClassName("price-check");
+
+    initAdvancedControls($advancedOptions);
+    initSort();
+    initFilter($types, false);
+    initFilter($prices, true);
+    initReset($types, $prices);
+    initSave();
   }
 
   initPlaces();
